b7ffc53ab05d8f746a2ddff6648a717f021d17dd
Ritu Verma <59959029+Rituverma12@users.noreply.github.com>
2020-06-05 15:17:30 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,41 +1,40 @@
-
+<html>
+<body>
 <?php 
+
 $link = mysqli_connect("localhost", "root", "");
 mysqli_select_db($link,"practice");
 //get values from form in signup file
 
 if(isset($_POST["log-in"])){
-	
+	$errors = array();
 	$username = $_POST["username"];
-	//$email = $POST['email'];
 	$password = md5($_POST["password"]);
-	//to prevent mysql injection
 	$username = stripcslashes($username);
-	//$email = stripcslashes($email);
 	$password = stripcslashes($password);
 	$username = mysqli_real_escape_string($link,$username);
 	$password = mysqli_real_escape_string($link,$password);
-
-	//connect to the server and select database
-
-	//query the database for user
+	
 	$result = mysqli_query($link,"SELECT * from `user` where `username` = '$username' and `password` = '$password'")
 	                or die("Failed to query database".mysqli_error($link));
 	$row = mysqli_fetch_array($result);
 
 	if($row['username'] == $username && $row['password'] == $password)
 	{
-	   session_start();
-	    $_SESSION['username']=$username;
+	  session_start();
+	  $_SESSION['username']=$username;
 	  header("Location: apihere.php");
 
 	}
 	else{
-
-	   header("Location: SCROLL.php");
-	   echo "Invalid username or password, please try again!";
+		array_push($errors, "Invalid Username/Password. Please try again!");
+		
+	   include 'errors.php';
+	   
 	}
 }
 
 
 ?>
+</body>
+</html>
